6cffe3499fd07e37bba82eecc484e6a0ae1f066f
Tatraa <tatra5342@gmail.com>
2023-10-10 18:40:08 +0200

    Presentation - Sentiment Analysis 1.0

--- Presentation/learn3.py
+++ Presentation/learn3.py
@@ -13,18 +13,8 @@ def opinion(x, positive_count, negative_count, neutral_count):
         neutral_count += 1
     return positive_count, negative_count, neutral_count
 
-tekst = "It's a really good product! I wish every scarf was that comfy!"
-tekst2 = "I've never bought such a bad scarf, it's very itchy"
-tekst3 = "Pretty nice product, really comfy and really warm"
-tekst4 = "Terrible!"
-tekst5 = "Best product!"
-tekst6 = "Scarf is really cozy, nice coloring"
-tekst7 = "Worthy buying"
-tekst8 = "I don't know..."
-tekst9 = "Awesome, but not very various in coloring"
-tekst10 = "It was worth the wait"
-
-opinions = [tekst, tekst2, tekst3, tekst4, tekst5, tekst6, tekst7, tekst8, tekst9, tekst10]
+with open('text.txt', 'r') as file:
+    opinions = file.readlines()
 
 positive_count = 0
 negative_count = 0
